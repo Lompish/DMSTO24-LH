80ed879f226adacb2c48f28a820ebab43f920c39
Lompish <linda.hiermann.data24sto@edu.tucsweden.se>
2024-12-17 11:12:23 +0100

    Test

---
+++ Private/helloworld.py
@@ -0,0 +1,52 @@
+print("Hello, World!")
+
+name = "Linda"
+age = 31 
+print (f"Jag heter {name} och jag är {age} år gammal!")
+
+x = 10
+y = 20
+summa = x + y
+print (f"Den totala summan av x och y är {summa} och jag definerar den som z = {summa}")
+
+
+heltal = 10
+flyttal = 3.5
+text = "Jag är bäst!"
+sanningsvärde = 'true'
+print(type (heltal))
+print(type (flyttal))
+print(type (text))
+print(type (sanningsvärde))
+
+x = 15
+y = str(x)
+print (y)
+print (type (y))
+
+y = 42.4
+x = float(y)
+print (x)
+print (type (x))
+
+y = 42.5
+x = int(y)
+print (x)
+print (type (x))
+
+
+x = "123"
+y = 2 
+print (int (x) * (y))
+
+x = "123"
+y = 2 
+z = int (x)
+summa = z * y 
+print (f"Svaret är detta {summa}")
+
+x = "123"
+y = 2 
+z = int (x)
+print (f"Svaret {z * y}")
+
